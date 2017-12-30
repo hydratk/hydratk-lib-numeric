fbc41d81af93170cfefa5a59d13a6e12d65a13b8
Petr Rašek <bowman@hydratk.org>
2017-12-30 17:21:52 +0100

    Modified - year 2018

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'hydratk-lib-numeric'
-copyright = u'2017, HydraTK Team'
+copyright = u'2017-2018, HydraTK Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
